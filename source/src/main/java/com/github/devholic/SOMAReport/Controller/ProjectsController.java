6cef485817ca33dd80035da34fc92c7a4cb7b630
Min JJong <alswhdgus10@naver.com>
2015-07-28 16:47:53 +0900

    gg

--- source/src/main/java/com/github/devholic/SOMAReport/Controller/ProjectsController.java
+++ source/src/main/java/com/github/devholic/SOMAReport/Controller/ProjectsController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.github.devholic.SOMAReport.Model.Projects;
 
@@ -131,21 +132,23 @@ public class ProjectsController {
 	}
 	
 	@PUT
-	public void updateProject(){
+	public Response updateProject(){
 		try{
-			
+			return Response.status(200).type(MediaType.APPLICATION_JSON).entity("put : 200").build();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		return Response.status(500).type(MediaType.APPLICATION_JSON).entity("put : 500").build();
 	}
 	
 	@DELETE
-	public void deleteProject(){
+	public Response deleteProject(){
 		try{
-			
+			return Response.status(200).type(MediaType.APPLICATION_JSON).entity("delete : 200").build();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		return Response.status(500).type(MediaType.APPLICATION_JSON).entity("delete : 500").build();
 	}
 	
 	
