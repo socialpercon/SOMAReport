f70cbfbd75763e9ac59820f7c1203b4e47b9b7f7
devholic : MBPr (Mac) <devholic@plusquare.com>
2015-08-20 16:31:54 +0900

    Layout

--- source/src/main/java/com/github/devholic/SOMAReport/Project.java
+++ source/src/main/java/com/github/devholic/SOMAReport/Project.java
@@ -2,6 +2,7 @@ package com.github.devholic.SOMAReport;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
@@ -13,6 +14,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.github.devholic.SOMAReport.Controller.ProjectsController;
+import com.github.devholic.SOMAReport.Controller.ReportsController;
 import com.github.devholic.SOMAReport.Utilities.MustacheHelper;
 
 @Path("/")
@@ -28,15 +30,40 @@ public class Project {
 	public Response View_Project(@Context Request request) {
 		Session session = request.getSession();
 		if (session.getAttribute("user_id") != null) {
-			JSONArray userProject = ProjectsController.getMyProject(session.getAttribute(
-					"user_id").toString());
+			JSONArray userProject = ProjectsController.getMyProject(session
+					.getAttribute("user_id").toString());
 			JSONObject data = new JSONObject();
 			data.put("project", userProject);
 			Log.info(data.toString());
 			return Response
 					.status(200)
-					.entity(new Viewable("/user_project.mustache", MustacheHelper
-							.toMap(data))).build();
+					.entity(new Viewable("/new/new_projectlist.mustache",
+							MustacheHelper.toMap(data))).build();
+		} else {
+			return Response.status(401).entity(new Viewable("/login.mustache"))
+					.build();
+		}
+	}
+
+	@GET
+	@Path("/project/{id}")
+	public Response View_ProjectDetail(@Context Request request,
+			@PathParam("id") String id) {
+		Session session = request.getSession();
+		if (session.getAttribute("user_id") != null) {
+			// #66C4DE : waiting
+			// #FC6A6A : finish
+			ProjectsController projects = new ProjectsController();
+			JSONObject userProject = projects.getDetailByProjectId(id);
+			JSONObject data = new JSONObject();
+			data.put("project", userProject);
+			ReportsController reports = new ReportsController();
+			data.put("reports", reports.getReportByProjectId(id));
+			Log.info(data.toString());
+			return Response
+					.status(200)
+					.entity(new Viewable("/new/new_project.mustache",
+							MustacheHelper.toMap(data))).build();
 		} else {
 			return Response.status(401).entity(new Viewable("/login.mustache"))
 					.build();
