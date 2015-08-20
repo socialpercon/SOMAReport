deb120a8ae201789f1885cfb0883edc515a9387d
JaeYeonLee <jyegglee@gmail.com>
2015-08-21 04:19:13 +0900

    statistics view

--- source/src/main/java/com/github/devholic/SOMAReport/Console_Statistics.java
+++ source/src/main/java/com/github/devholic/SOMAReport/Console_Statistics.java
@@ -1,13 +1,19 @@
 package com.github.devholic.SOMAReport;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.glassfish.jersey.server.mvc.Viewable;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.mortbay.log.Log;
 
 import com.github.devholic.SOMAReport.Controller.DatabaseController;
 import com.github.devholic.SOMAReport.Controller.ProjectsController;
@@ -23,9 +29,12 @@ public class Console_Statistics {
 	DatabaseController db = new DatabaseController();
 	StatisticsController statC = new StatisticsController();
 	
+	@Context
+	UriInfo uri;
+	
 	@GET
 	@Path("/console/statistics")
-	public Response Vies_Statistics () {
+	public Response View_Statistics () {
 		JSONObject jo = new JSONObject();
 		
 		return Response
@@ -70,7 +79,7 @@ public class Console_Statistics {
 	
 	@GET
 	@Path("/console/statistics/perMonth")
-	public Response Vies_Statistics_perMonth () {
+	public Response View_Statistics_perMonths () {
 		JSONObject jo = new JSONObject();
 		
 		return Response
@@ -79,9 +88,31 @@ public class Console_Statistics {
 						MustacheHelper.toMap(jo))).build();
 	}
 	
+	@POST
+	@Path("/console/statistics/perMonth")
+	public Response View_Statistics_perMonth (@Context Request request, 
+			@FormDataParam("year") String year, 
+			@FormDataParam("month") String month) {
+		JSONObject jo = new JSONObject();
+		int years = Integer.parseUnsignedInt(year);
+		int months = Integer.parseUnsignedInt(month);
+		jo.put("year", year);
+		jo.put("month", month);
+		Log.info(years+" "+months);
+		JSONArray mentors = statC.totalMentoringInfoByMonth(years, months, UserController.ROLE_MENTOR);
+		jo.put("mentors", mentors);
+		JSONArray mentees = statC.totalMentoringInfoByMonth(years, months, UserController.ROLE_MENTEE);
+		jo.put("mentees", mentees);
+		Log.info(jo.toString());
+		return Response
+				.status(200)
+				.entity(new Viewable("/console_statistics_byMonth.mustache", 
+						MustacheHelper.toMap(jo))).build();
+	}
+	
 	@GET
 	@Path("/console/statistics/perMonth/{year}/{month}")
-	public Response Vies_Statistics_perMonth_data (@PathParam("year") String year, @PathParam("month")String month) {
+	public Response View_Statistics_perMonth_data (@PathParam("year") String year, @PathParam("month")String month) {
 		JSONObject jo = new JSONObject();
 		int years = Integer.parseUnsignedInt(year);
 		int months = Integer.parseUnsignedInt(month);
