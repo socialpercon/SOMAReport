36b10cf125194b800fb57aa0dacc6386a8fbee11
devholic : MBPr (Mac) <devholic@plusquare.com>
2015-08-21 16:04:21 +0900

    Layout modify

--- source/src/main/java/com/github/devholic/SOMAReport/Controller/UserController.java
+++ source/src/main/java/com/github/devholic/SOMAReport/Controller/UserController.java
@@ -24,11 +24,14 @@ public class UserController {
 		// Key : email
 		// Value : salt / value
 		System.out.println();
-		InputStream is = db.getByView("_design/user", "auth", email, false, false, false);
-		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory.inputStreamToString(is)));
+		InputStream is = db.getByView("_design/user", "auth", email, false,
+				false, false);
+		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory
+				.inputStreamToString(is)));
 		if (result.length() != 0) {
 			JSONArray parsed = JSONFactory.getValue(result.getJSONObject(0));
-			if (StringFactory.encryptPassword(password, parsed.getString(0)).equals(parsed.getString(1))) {
+			if (StringFactory.encryptPassword(password, parsed.getString(0))
+					.equals(parsed.getString(1))) {
 				return result.getJSONObject(0).getString("id");
 			} else {
 				return null;
@@ -42,8 +45,10 @@ public class UserController {
 		// _design/user/role
 		// Key : role
 		// Value : [ {id, value[_id / role / email / name / belong / years ]} ]
-		InputStream is = db.getByView("_design/user", "role", "mentee", false, false, false);
-		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory.inputStreamToString(is)));
+		InputStream is = db.getByView("_design/user", "role", "mentee", false,
+				false, false);
+		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory
+				.inputStreamToString(is)));
 		return result;
 	}
 
@@ -51,36 +56,45 @@ public class UserController {
 		// _design/user/role
 		// Key : role
 		// Value : [ {id, value[_id / role / email / name / belong / years ]} ]
-		InputStream is = db.getByView("_design/user", "role", "mentor", false, false, false);
-		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory.inputStreamToString(is)));
+		InputStream is = db.getByView("_design/user", "role", "mentor", false,
+				false, false);
+		JSONArray result = JSONFactory.getData(new JSONObject(StringFactory
+				.inputStreamToString(is)));
 		return result;
 	}
 
 	public static boolean isAlreadyRegistered(String email) {
 		// 이메일을 통해 이미 등록된 사용자인지를 확인한다
-		InputStream is = db.getByView("_design/user", "search_by_email", email, false, false, false);
-		JSONArray results = JSONFactory.getData(new JSONObject(StringFactory.inputStreamToString(is)));
-		if (results.length() == 0) 
+		InputStream is = db.getByView("_design/user", "search_by_email", email,
+				false, false, false);
+		JSONArray results = JSONFactory.getData(new JSONObject(StringFactory
+				.inputStreamToString(is)));
+		if (results.length() == 0)
 			return false;
 		else
 			return true;
 	}
-	
-	public static JSONObject getUserByEmail (String email) {
-		// 이메일을 통해 사용자 문서를 가져온다 
-		JSONArray results = JSONFactory.getData(JSONFactory.inputStreamToJson(db.getByView("_design/user", "search_by_email", email, true, false, false)));
-		if (results.length() == 0) 
-			return null; 
+
+	public static JSONObject getUserByEmail(String email) {
+		// 이메일을 통해 사용자 문서를 가져온다
+		JSONArray results = JSONFactory.getData(JSONFactory
+				.inputStreamToJson(db.getByView("_design/user",
+						"search_by_email", email, true, false, false)));
+		if (results.length() == 0)
+			return null;
 		else
 			return results.getJSONObject(0).getJSONObject("doc");
 	}
 
 	public static String getIdbyName(String name) {
 		// 이름을 통해 해당 사용자 문서의 _id를 가져온다
-		InputStream is =db.getByView("_design/user", "search_by_name", name, false, false, false);
+		InputStream is = db.getByView("_design/user", "search_by_name", name,
+				false, false, false);
 		try {
-			JSONArray results = JSONFactory.inputStreamToJson(is).getJSONArray("rows");
-			if (results.length() == 0)	throw new Exception();
+			JSONArray results = JSONFactory.inputStreamToJson(is).getJSONArray(
+					"rows");
+			if (results.length() == 0)
+				throw new Exception();
 			return results.getJSONObject(0).getString("value");
 		} catch (IOException e) {
 			Log.error(e.getLocalizedMessage());
@@ -97,7 +111,7 @@ public class UserController {
 	 * @param userId
 	 * @return int (ROLE_MENTOR / ROLE_MENTEE)
 	 */
-	public int getRoleById (String userId) {
+	public int getRoleById(String userId) {
 		JSONObject userDoc = JSONFactory.inputStreamToJson(db.getDoc(userId));
 		if (userDoc.has("role")) {
 			if (userDoc.getString("role").equals("mentor"))
@@ -108,34 +122,34 @@ public class UserController {
 				Log.error("Wrong role info");
 				return -1;
 			}
-		}
-		else {
+		} else {
 			Log.error("Wrong role info");
 			return -1;
 		}
 	}
-	
+
 	/**
 	 * _id에 맞는 이름을 가져온다.
 	 * 
 	 * @param userId
 	 * @return String (user name)
 	 */
-	public static String getUserName(String userId) {
+	public String getUserName(String userId) {
 		JSONObject userDoc = JSONFactory.inputStreamToJson(db.getDoc(userId));
 		return userDoc.getString("name");
 	}
-	
-	public JSONArray getUsersInYear (int year) {
-		JSONObject raw = JSONFactory.inputStreamToJson(db.getByView("_design/user", "users_in_year", year, false, true, false));
-		JSONArray result = JSONFactory.getData(raw);	
+
+	public JSONArray getUsersInYear(int year) {
+		JSONObject raw = JSONFactory.inputStreamToJson(db.getByView(
+				"_design/user", "users_in_year", year, false, true, false));
+		JSONArray result = JSONFactory.getData(raw);
 		JSONArray users = new JSONArray();
-		for (int i=0; i<result.length(); i++) {
+		for (int i = 0; i < result.length(); i++) {
 			users.put(result.getJSONObject(i).get("value"));
 		}
 		return users;
 	}
-	
+
 	/**
 	 * 해당 유저의 기본 정보 (name, id, belong)를 가져온다.
 	 * 
@@ -144,11 +158,11 @@ public class UserController {
 	public static JSONObject getUserInfo(String userId) {
 		JSONObject userDoc = new JSONObject();
 		JSONObject user = JSONFactory.inputStreamToJson(db.getDoc(userId));
-		
+
 		userDoc.put("id", user.get("_id"));
 		userDoc.put("name", user.get("name"));
 		userDoc.put("belong", user.get("belong"));
-		
+
 		return userDoc;
 	}
 }
